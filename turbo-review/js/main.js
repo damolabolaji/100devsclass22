2048020924be37087181a9f14b8b1450862af945
damolabolaji <bolaji.oyindamola@gmail.com>
2022-10-25 12:59:15 +0100

    added apple exercise with a program that logs the occurences of all words in the string

--- turbo-review/js/main.js
+++ turbo-review/js/main.js
@@ -1,9 +1,35 @@
 // *Variables*
 // Declare a variable and assign it to your fav drink as a string. Make sure there is no whitespace on either side of the string, and print the value to the console
 
+let favDrink = '      pineapple and papaya smoothies'
 
+console.log(favDrink.trim())
 
 //Declare a variable, assign it a string of multiple words, and check to see if one of the words is "apple".
+
+let wordString = 'mango apple pineapple sweet walk run apple mango car stop window car man every'
+let searchWord = prompt('enter a word to search for in the string')
+
+console.log(wordString.search(searchWord))
+
+if(wordString.search(searchWord) === -1){
+    console.log(`there is no ${searchWord}`)
+}else{
+    console.log(`we have ${searchWord}`)
+}
+
+let sumOfItems = wordString.split(' ').reduce( function (object,item)  {
+      if(!object[item]){
+        object[item] = 0
+    }
+    object[item]++
+    return object
+}, {}
+  
+)
+
+console.log(sumOfItems)
+
 
 // *Functions*
 // Create a function that returns rock, paper, or scissors as randomly as possible
