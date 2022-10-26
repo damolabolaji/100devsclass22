4d74e749bf85177c1caea4ed32de0efc4910d889
damolabolaji <bolaji.oyindamola@gmail.com>
2022-10-26 13:27:24 +0100

    added conditionals solution

--- turbo-review/js/main.js
+++ turbo-review/js/main.js
@@ -7,28 +7,28 @@ console.log(favDrink.trim())
 
 //Declare a variable, assign it a string of multiple words, and check to see if one of the words is "apple".
 
-let wordString = 'mango apple pineapple sweet walk run apple mango car stop window car man every'
-let searchWord = prompt('enter a word to search for in the string')
+// let wordString = 'mango apple pineapple sweet walk run apple mango car stop window car man every'
+// let searchWord = prompt('enter a word to search for in the string')
 
-console.log(wordString.search(searchWord))
+// console.log(wordString.search(searchWord))
 
-if(wordString.search(searchWord) === -1){
-    console.log(`there is no ${searchWord}`)
-}else{
-    console.log(`we have ${searchWord}`)
-}
+// if(wordString.search(searchWord) === -1){
+//     console.log(`there is no ${searchWord}`)
+// }else{
+//     console.log(`we have ${searchWord}`)
+// }
 
-let sumOfItems = wordString.split(' ').reduce( function (object,item)  {
-      if(!object[item]){
-        object[item] = 0
-    }
-    object[item]++
-    return object
-}, {}
+// let sumOfItems = wordString.split(' ').reduce( function (object,item)  {
+//       if(!object[item]){
+//         object[item] = 0
+//     }
+//     object[item]++
+//     return object
+// }, {}
   
-)
+// )
 
-console.log(sumOfItems)
+// console.log(sumOfItems)
 
 
 // *Functions*
@@ -37,11 +37,11 @@ function rockPaperScissors() {
     randomNumber = Math.random();
 
     if(randomNumber < 0.33){
-        console.log('rock')
+        return 'rock'
     }else if(randomNumber < 0.66 ){
-        console.log('paper')
+        return 'paper'
     }else{
-        console.log('scissors')
+        return 'scissors'
     }
 }
 rockPaperScissors()
@@ -49,6 +49,29 @@ rockPaperScissors()
 
 // *Conditionals*
 //Create a function that takes in a choice (rock, paper, or scissors) and determines if they won a game of rock paper scissors against a bot using the above function
+//player - bot
+//rock - scissor = player wins
+//scissors - paper = player wins
+//paper - scissors = bot wins
+//
+
+let playerChoice = prompt('rock or Paper or Scissors')
+
+function gameWithBot(){
+    if(rockPaperScissors() === playerChoice ){
+        console.log('You Tied')
+    }
+    else if ((rockPaperScissors() === 'scissors' && playerChoice === 'rock') ||
+    (rockPaperScissors() === 'paper' && playerChoice === 'scissors') ||
+    (rockPaperScissors() === 'scissors' && playerChoice === 'rock') ) {
+        console.log('You Win')
+    }else{
+        console.log('You Lose')
+    }
+
+
+}
+gameWithBot()
 
 //*Loops*
 //Create a function that takes an array of choices. Play the game x times where x is the number of choices in the array. Print the results of each game to the console.
