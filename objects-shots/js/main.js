12615f159d19c0ae926556bb03e90b804696d1b8
damolabolaji <bolaji.oyindamola@gmail.com>
2022-10-29 13:21:20 +0100

    added API and carousel drink view. homework for class 26

--- objects-shots/js/main.js
+++ objects-shots/js/main.js
@@ -1,16 +1,59 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
 
 
-//javascript info - Objects
+
+document.querySelector('#search').addEventListener('click', getValue)
+
+      
+function getValue(){
+  let searchValue = document.querySelector('input').value.split(' ').join('')
 
 
-function spinWords(string){
-    //TODO Have fun :)
-    
-    splitString = string.split(' ').map(item => item.length < 5 ? item : item.split('').reverse().join('')).join(' ');
 
-       console.log(splitString)
-   
+  
+fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchValue}`)
+.then(res => res.json()) // parse response as JSON
+.then(data => {
+  console.log(data)
+
+  let drinkIndex = 0
+  document.querySelector('img').src = data.drinks[drinkIndex].strDrinkThumb;
+  document.querySelector('h2').innerText = data.drinks[drinkIndex].strDrink
+  document.querySelector('h3').innerText = data.drinks[drinkIndex].strInstructions
+
+  document.querySelector('#right-button').addEventListener('click', switchValuesMore)
+  function switchValuesMore(){
+
+    if(drinkIndex < data.drinks.length-1){
+      drinkIndex++
+      document.querySelector('img').src = data.drinks[drinkIndex].strDrinkThumb;
+      document.querySelector('h2').innerText = data.drinks[drinkIndex].strDrink
+      document.querySelector('h3').innerText = data.drinks[drinkIndex].strInstructions
+    }
+  
   }
 
-  spinWords('i love grilling some chicken')
+  document.querySelector('#left-button').addEventListener('click', switchValuesLess)
+  function switchValuesLess(){
+    if(drinkIndex > 0){
+      drinkIndex--
+      document.querySelector('img').src = data.drinks[drinkIndex].strDrinkThumb;
+      document.querySelector('h2').innerText = data.drinks[drinkIndex].strDrink
+      document.querySelector('h3').innerText = data.drinks[drinkIndex].strInstructions
+    }
+   
+  }
+  
+
+})
+.catch(err => {
+    console.log(`error ${err}`)
+});
+
+
+}
+
+
+
+
+
